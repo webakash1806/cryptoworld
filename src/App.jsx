f320b55fa87ea680337bc397739fd4d0e2b6cae5
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-09-13 22:05:10 +0530

    api links

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ const App = () => {
   return (
     <div>
       Hello
+      {/* mainlink : "https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1" */}
+      {/* graph link : "https://www.coingecko.com/coins/{id}/sparkline.svg" */}
     </div>
   )
 }
