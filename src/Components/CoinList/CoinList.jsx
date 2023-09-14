94ea88ec55525119884a6705563cf430f9013110
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-09-14 20:02:16 +0530

    Coin List styles updated

--- src/Components/CoinList/CoinList.jsx
+++ src/Components/CoinList/CoinList.jsx
@@ -9,9 +9,22 @@ const CoinList = () => {
     console.log(value)
 
     return (
-        <>
+        <div className="w-fit">
             <h1 className="">COIN DATA</h1>
-            <div className="">
+            <div className="flex flex-col gap-2 ">
+                <div className="flex font-bold capitalize border-t-[1px] border-b-[1px] pt-3 pb-3">
+                    <p className="min-w-[5rem] text-center ">rank</p>
+                    <div className="min-w-[17.7rem]  text-left md:min-w-[20.7rem]">
+                        <p >name</p>
+                    </div>
+                    <div className="flex gap-3">
+                        <p className="min-w-[8rem] text-right ">price</p>
+                        <p className="min-w-[7rem] text-right ">low24</p>
+                        <p className="min-w-[7rem] text-right ">high24</p>
+                        <p className="min-w-[5rem] text-right ">price %</p>
+                        <p className="min-w-[10rem] text-right">marketCap</p>
+                    </div>
+                </div>
                 {value.map((val) => <CoinListBox
                     key={val.id}
                     rank={val.market_cap_rank}
@@ -24,7 +37,7 @@ const CoinList = () => {
                     priceChangePer={val.market_cap_change_percentage_24h}
                     marketCap={val.market_cap} />)}
             </div>
-        </>
+        </div>
     )
 }
 
--- src/Components/CoinListBox.jsx
+++ src/Components/CoinListBox.jsx
@@ -1,22 +1,25 @@
 const CoinListBox = (props) => {
     const { id, name, image, price, low24, high24, priceChangePer, marketCap, rank, symbol } = props
     return (
-        <div key={id} className="flex items-center justify-left">
-            <p className="w-[3rem] text-center border">{rank}.</p>
-            <img src={image} alt="" className="w-8" />
-            <div className="w-[10rem] text-left border">
-                <p >{name}</p>
-                <p>{symbol}</p>
-            </div>
+        <>
+            <div key={id} className="flex items-center justify-left border-b-[1px] pt-2.5 pb-2.5">
 
-            <div className="flex gap-3">
-                <p className="w-[8rem] text-right border">${price.toFixed(2)}</p>
-                <p className="text-red-500 w-[7rem] text-right border">${low24.toFixed(2)}</p>
-                <p className="w-[7rem] text-green-600 text-right border">${high24.toFixed(2)}</p>
-                <p className={(priceChangePer < 0) ? `text-red-500 w-[5rem] text-right border` : `text-green-600 w-[5rem] text-right border`}>{priceChangePer.toFixed(2)}%</p>
-                <p className="w-[10rem] text-right border">${marketCap}</p>
+                <p className="min-w-[5rem] text-center ">{rank}.</p>
+                <img src={image} alt="" className="w-[2rem] mr-3" />
+                <div className="min-w-[15rem] text-left  flex flex-col md:flex-row md:gap-3 md:min-w-[18rem]">
+                    <p className="text-[17px]">{name}</p>
+                    <p className="text-[#505050] uppercase text-[13px]">{symbol}</p>
+                </div>
+
+                <div className="flex gap-3">
+                    <p className="min-w-[8rem] text-right ">${price.toFixed(2)}</p>
+                    <p className="text-red-500 min-w-[7rem] text-right ">${low24.toFixed(2)}</p>
+                    <p className="min-w-[7rem] text-green-600 text-right ">${high24.toFixed(2)}</p>
+                    <p className={(priceChangePer < 0) ? `text-red-500 min-w-[5rem] text-right ` : `text-green-600 min-w-[5rem] text-right `}>{priceChangePer.toFixed(2)}%</p>
+                    <p className="min-w-[10rem] text-right ">${marketCap}</p>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
