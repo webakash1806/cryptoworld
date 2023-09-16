663052651663f70e7b91ae8bdf4adef5d92e1b70
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-09-16 14:18:56 +0530

    Repo link updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,24 @@ import React from 'react'
 import ListContextData from './Context/ListContextProvider'
 import CoinList from './Components/CoinList/CoinList'
 import ExchangeList from './Components/ExchangeList/ExchangeList'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Header from './Components/Header/Header'
 
 const App = () => {
   return (
     <ListContextData>
 
-      <BrowserRouter>
 
-        <Routes>
-          Hello
+      <Routes>
+        Hello
 
-          <Route path="/cryptoworld" element={<Header />} />
-          <Route path="/cryptoworld/exchange" element={<ExchangeList />} />
-          <Route path="/cryptoworld/coin" element={<CoinList />} />
+        <Route path="/cryptoworld" element={<Header />} />
+        <Route path="/cryptoworld/exchange" element={<ExchangeList />} />
+        <Route path="/cryptoworld/coin" element={<CoinList />} />
 
-          {/* mainlink : "https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1" */}
-          {/* graph link : "https://www.coingecko.com/coins/{id}/sparkline.svg" */}
-        </Routes>
-      </BrowserRouter>
+        {/* mainlink : "https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1" */}
+        {/* graph link : "https://www.coingecko.com/coins/{id}/sparkline.svg" */}
+      </Routes>
 
     </ListContextData>
   )
