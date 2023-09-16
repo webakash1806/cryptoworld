12da254679b8d78700cce4ee10118c924b724b45
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-09-16 14:09:44 +0530

    Repo link updated

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,11 @@
-import { useContext } from "react"
 import { Link } from "react-router-dom"
-import ListContext from "../../Context/ListContext"
 
 const Header = () => {
-    const { list, setList } = useContext(ListContext)
-    const value = list
-    console.log(value)
+
     return (
         <div>
-            <Link to="/CryptoWorld/exchange" onClick={() => setList({ ...list, excActive: 'exc' })}>Exchange</Link>
-            <Link to="/CryptoWorld/coin" onClick={() => setList({ ...list, coinActive: 'coin' })}>Coins</Link>
+            <Link to="/cryptoworld/exchange">Exchange</Link>
+            <Link to="/cryptoworld/coin">Coins</Link>
         </div>
     )
 }
