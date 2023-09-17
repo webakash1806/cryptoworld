b5934bccb642eb800597fa70b540ef38126b011e
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-09-17 14:05:16 +0530

    Coin list responsive design created

--- src/Components/CoinList/CoinList.jsx
+++ src/Components/CoinList/CoinList.jsx
@@ -12,32 +12,34 @@ const CoinList = () => {
         <div className=" flex pt-6 pb-6 flex-col items-center justify-center">
             <Header />
             <h1 className="">COIN DATA</h1>
-            <div className="flex flex-col gap-2 ">
+            <div className="flex flex-col p-2">
                 <div className="flex pt-3 pb-3">
-                    <div className="flex flex-col min-w-[25rem] ">
-                        <div className="flex font-bold capitalize border-t-[1px] border-b-[1px] pt-4 pb-4">
-                            <p className="min-w-[5rem] text-center ">rank</p>
-                            <div className="min-w-[17.7rem]  text-left md:min-w-[20.7rem]">
+                    <div className="flex flex-col min-w-[10rem] md:min-w-[20rem] lg:min-w-[25rem]  ">
+                        <div className="flex font-bold text-[14px] sm:text-[17x] capitalize border-t-[1px] border-b-[1px] pt-4 pb-4">
+                            <p className="min-w-[2rem] md:min-w-[5rem] text-center ">#</p>
+                            <div className="min-w-[10em] md:min-w-[15.5rem] lg:min-w-[17.7rem] text-left">
                                 <p >name</p>
                             </div>
                         </div>
-                        {list.coinList.map((val) => <CoinListBox
-                            key={val.id}
-                            rank={val.market_cap_rank}
-                            name={val.name}
-                            symbol={val.symbol}
-                            image={val.image}
-                        />)}
-                    </div>
-                    <div className="flex flex-col max-w-[30rem] lg:max-w-fit lg:overflow-hidden overflow-x-scroll">
-                        <div className="flex font-bold gap-3 capitalize border-t-[1px] border-b-[1px] pt-4 pb-4 w-fit">
-                            <p className="min-w-[8rem] text-right ">price</p>
-                            <p className="min-w-[7rem] text-right ">low24</p>
-                            <p className="min-w-[7rem] text-right ">high24</p>
-                            <p className="min-w-[5rem] text-right ">price %</p>
-                            <p className="min-w-[10rem] text-right">marketCap</p>
+                        <div className="text-[14px] md:text-[15px] lg:text-[16px]">
+                            {list.coinList.map((val) => <CoinListBox
+                                key={val.id}
+                                rank={val.market_cap_rank}
+                                name={val.name}
+                                symbol={val.symbol}
+                                image={val.image}
+                            />)}
                         </div>
-                        <div>
+                    </div>
+                    <div className="flex flex-col max-w-[7.5rem] sm:max-w-[22rem] md:max-w-[26rem] lg:max-w-fit lg:overflow-hidden overflow-x-scroll">
+                        <div className="flex font-bold text-[14px] sm:text-[17x] capitalize border-t-[1px] border-b-[1px] pt-4 pb-4 w-fit">
+                            <p className="min-w-[5.5rem] md:min-w-[7rem] lg:min-w-[8rem] text-right ">price</p>
+                            <p className="min-w-[5.5rem] sm:min-w-[7rem] text-right ">low24</p>
+                            <p className="min-w-[6rem] sm:min-w-[7rem] text-right ">high24</p>
+                            <p className="min-w-[4rem] sm:min-w-[5rem] text-right ">%</p>
+                            <p className="min-w-[10rem] text-right pr-4">marketCap</p>
+                        </div>
+                        <div className="text-[13px] md:text-[15px] lg:text-[16px]">
                             {list.coinList.map((val) => <CoinListScroll
                                 key={val.id}
                                 price={val.current_price}
--- src/Components/CoinList/CoinListBox.jsx
+++ src/Components/CoinList/CoinListBox.jsx
@@ -3,12 +3,12 @@ const CoinListBox = (props) => {
     return (
         <>
             <div key={id} className="flex items-center justify-left border-b-[1px] pt-2.5 pb-2.5 h-[3.5rem]">
-                <div className="flex items-center justify-left">
-                    <p className="min-w-[5rem] text-center ">{rank}.</p>
-                    <img src={image} alt="" className="w-[2rem] mr-3" />
-                    <div className="min-w-[15rem] text-left  flex flex-col lg:flex-row lg:gap-3 md:min-w-[18rem]">
-                        <p className="text-[17px] font-semibold">{name}</p>
-                        <p className="text-[#505050] uppercase text-[12px]">{symbol}</p>
+                <div className="flex items-center justify-left ">
+                    <p className="min-w-[2rem] md:min-w-[5rem] text-center">{rank}.</p>
+                    <img src={image} alt="" className="w-[1.3rem] sm:w-[2rem] mr-3" />
+                    <div className="max-w-[8rem] sm:max-w-[15rem] text-left  flex flex-col lg:flex-row lg:gap-3 md:max-w-[18rem]">
+                        <p className=" font-semibold ">{name}</p>
+                        <p className="text-[#505050] uppercase text-[9.5px] lg:text-[12px]">{symbol}</p>
                     </div>
                 </div>
             </div>
