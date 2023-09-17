632d43e638891aae045f5147154467152167256b
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-09-17 11:14:44 +0530

    Coin list scroll added

--- src/Components/CoinList/CoinList.jsx
+++ src/Components/CoinList/CoinList.jsx
@@ -3,41 +3,51 @@ import { useContext } from "react"
 import ListContext from "../../Context/ListContext"
 import CoinListBox from "./CoinListBox"
 import CoinPagination from "../Pagination/CoinPagination"
+import CoinListScroll from "./CoinListScroll"
 
 const CoinList = () => {
     const { list, setList } = useContext(ListContext)
 
     return (
-        <div className="w-fit flex pt-6 pb-6 flex-col items-center justify-center">
+        <div className=" flex pt-6 pb-6 flex-col items-center justify-center">
             <Header />
             <h1 className="">COIN DATA</h1>
             <div className="flex flex-col gap-2 ">
-                <div className="flex font-bold capitalize border-t-[1px] border-b-[1px] pt-3 pb-3">
-                    <p className="min-w-[5rem] text-center ">rank</p>
-                    <div className="min-w-[17.7rem]  text-left md:min-w-[20.7rem]">
-                        <p >name</p>
+                <div className="flex pt-3 pb-3">
+                    <div className="flex flex-col min-w-[25rem] ">
+                        <div className="flex font-bold capitalize border-t-[1px] border-b-[1px] pt-4 pb-4">
+                            <p className="min-w-[5rem] text-center ">rank</p>
+                            <div className="min-w-[17.7rem]  text-left md:min-w-[20.7rem]">
+                                <p >name</p>
+                            </div>
+                        </div>
+                        {list.coinList.map((val) => <CoinListBox
+                            key={val.id}
+                            rank={val.market_cap_rank}
+                            name={val.name}
+                            symbol={val.symbol}
+                            image={val.image}
+                        />)}
                     </div>
-                    <div className="flex gap-3">
-                        <p className="min-w-[8rem] text-right ">price</p>
-                        <p className="min-w-[7rem] text-right ">low24</p>
-                        <p className="min-w-[7rem] text-right ">high24</p>
-                        <p className="min-w-[5rem] text-right ">price %</p>
-                        <p className="min-w-[10rem] text-right">marketCap</p>
+                    <div className="flex flex-col max-w-[30rem] lg:max-w-fit lg:overflow-hidden overflow-x-scroll">
+                        <div className="flex font-bold gap-3 capitalize border-t-[1px] border-b-[1px] pt-4 pb-4 w-fit">
+                            <p className="min-w-[8rem] text-right ">price</p>
+                            <p className="min-w-[7rem] text-right ">low24</p>
+                            <p className="min-w-[7rem] text-right ">high24</p>
+                            <p className="min-w-[5rem] text-right ">price %</p>
+                            <p className="min-w-[10rem] text-right">marketCap</p>
+                        </div>
+                        <div>
+                            {list.coinList.map((val) => <CoinListScroll
+                                key={val.id}
+                                price={val.current_price}
+                                low24={val.low_24h}
+                                high24={val.high_24h}
+                                priceChangePer={val.market_cap_change_percentage_24h}
+                                marketCap={val.market_cap} />)}
+                        </div>
                     </div>
                 </div>
-                {list.coinList.map((val) => <CoinListBox
-                    key={val.id}
-                    rank={val.market_cap_rank}
-                    name={val.name}
-                    symbol={val.symbol}
-                    image={val.image}
-                    price={val.current_price}
-                    low24={val.low_24h}
-                    high24={val.high_24h}
-                    priceChangePer={val.market_cap_change_percentage_24h}
-                    marketCap={val.market_cap} />)}
-
-
             </div>
             <div className="flex">
                 {list.pageValue.map((val) => <CoinPagination key={val}
--- src/Components/CoinList/CoinListBox.jsx
+++ src/Components/CoinList/CoinListBox.jsx
@@ -1,22 +1,15 @@
 const CoinListBox = (props) => {
-    const { id, name, image, price, low24, high24, priceChangePer, marketCap, rank, symbol } = props
+    const { id, name, image, rank, symbol } = props
     return (
         <>
-            <div key={id} className="flex items-center justify-left border-b-[1px] pt-2.5 pb-2.5">
-
-                <p className="min-w-[5rem] text-center ">{rank}.</p>
-                <img src={image} alt="" className="w-[2rem] mr-3" />
-                <div className="min-w-[15rem] text-left  flex flex-col md:flex-row md:gap-3 md:min-w-[18rem]">
-                    <p className="text-[17px]">{name}</p>
-                    <p className="text-[#505050] uppercase text-[13px]">{symbol}</p>
-                </div>
-
-                <div className="flex gap-3">
-                    <p className="min-w-[8rem] text-right ">${price.toFixed(2)}</p>
-                    <p className="text-red-500 min-w-[7rem] text-right ">${low24.toFixed(2)}</p>
-                    <p className="min-w-[7rem] text-green-600 text-right ">${high24.toFixed(2)}</p>
-                    <p className={(priceChangePer < 0) ? `text-red-500 min-w-[5rem] text-right ` : `text-green-600 min-w-[5rem] text-right `}>{priceChangePer.toFixed(2)}%</p>
-                    <p className="min-w-[10rem] text-right ">${marketCap}</p>
+            <div key={id} className="flex items-center justify-left border-b-[1px] pt-2.5 pb-2.5 h-[3.5rem]">
+                <div className="flex items-center justify-left">
+                    <p className="min-w-[5rem] text-center ">{rank}.</p>
+                    <img src={image} alt="" className="w-[2rem] mr-3" />
+                    <div className="min-w-[15rem] text-left  flex flex-col lg:flex-row lg:gap-3 md:min-w-[18rem]">
+                        <p className="text-[17px] font-semibold">{name}</p>
+                        <p className="text-[#505050] uppercase text-[12px]">{symbol}</p>
+                    </div>
                 </div>
             </div>
         </>
