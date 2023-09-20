d6beb37c9d005cba0dcbccdd8a3656770cadb459
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-09-20 15:22:32 +0530

    Header responsiveness created

--- src/Components/Header/MainHeader/Header.jsx
+++ src/Components/Header/MainHeader/Header.jsx
@@ -21,18 +21,20 @@ const Header = () => {
 
 
     return (
-        <header className='flex w-screen bg-[#8247E5] text-white items-center justify-around z-[100] pt-[9px] pb-[9px] shadow-[-1px_1px_7px_#9b3bfb] fixed top-0 left-0'>
+        <header className='flex w-screen md:justify-between  md:pl-14 md:pr-6 lg:pl-16 lg:pr-8 lg:p-2 bg-[#8247E5] text-white items-center justify-around z-[100] pt-[9px] pb-[9px] shadow-[-1px_1px_7px_#9b3bfb] fixed top-0 left-0'>
             <div className=' w-[2rem] h-[2rem] flex items-center justify-center md:hidden cursor-pointer' onClick={hamActive}>
                 {barActive ? <FontAwesomeIcon icon={faBars} className='text-[1.3rem]' />
                     : <FontAwesomeIcon icon={faXmark} className='text-[1.6rem]' />}
             </div>
-            <div>LOGO</div>
-            <nav className={`bg-[#8247E5] text-white md:border-0 flex border-t-grey border-t-2  p-4 pb-10 md:p-0 w-screen md:w-fit items-center justify-center gap-3 md:gap-0 flex-col absolute 
+            <div className='md:mr-6 lg:mr-10'>LOGO</div>
+            <nav className={`bg-[#8247E5] text-white md:border-0 flex border-t-grey border-t-2  p-4 pb-10 md:p-0 w-screen md:w-[80vw] lg:w-[80vw] items-center justify-between gap-3 md:gap-0 flex-col absolute 
             ${barActive ? "top-[3.12rem] left-[-55rem] duration-[0.5s]" : "top-[3.12rem] left-0 duration-[0.5s]"}
              md:flex-row md:static `}>
-                {headerList.map((val) => <HeaderList key={val.list} url={val.url} list={val.list} />)}
+                <div className="flex flex-col md:flex-row">
+                    {headerList.map((val) => <HeaderList key={val.list} url={val.url} list={val.list} />)}
 
-                <button className='w-[90vw] bg-darkBg rounded-md md:w-fit p-3 mt-6 md:m-0 md:p-0 hover:bg-[#242424] duration-300'>Login/Signup</button>
+                </div>
+                <button className='w-[90vw] bg-darkBg rounded-md md:w-fit p-3 mt-6 md:m-0 md:mr-4 md:p-[6px_16px] md:rounded-[4px] md hover:bg-[#242424] duration-300'>Login/Signup</button>
             </nav>
             <DarkModeToggle />
         </header>
--- src/Components/Header/MainHeader/HeaderList.jsx
+++ src/Components/Header/MainHeader/HeaderList.jsx
@@ -3,7 +3,15 @@ import { Link } from 'react-router-dom'
 
 const HeaderList = ({ list }) => {
     return (
-        <div className='text-[1.1rem] font-semibold tracking-wide w-[90vw] md:w-fit text-center pb-4 pt-1 border-grey hover:border-white duration-300 cursor-pointer border-b-[1px] md:border-0'>
+        <div className='text-[1.1rem] 
+        font-[400] tracking-wide 
+        w-[90vw] md:w-fit text-center md:border-0
+        md:hover:border-b-2
+        pb-4 pt-1 md:p-[3px_8px] md:m-[0px_7px]
+        lg:p-[3px_9px] lg:m-[0px_12px]
+         border-grey hover:border-white 
+         duration-200 cursor-pointer 
+         border-b-[1px]'>
             <Link>{list}</Link>
         </div>
     )
