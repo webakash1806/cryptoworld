f47908e928b89c9fcc25826451067041dcf8bf3b
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-09-21 19:41:27 +0530

    Footer Page created

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='text-center mt-5 h-[10rem] flex items-center justify-center text-white bg-[#0C161B] shadow-[-1px_-2px_7px_#000]'>
+            Footer Section Coming Soon...
+        </div>
+    )
+}
+
+export default Footer
--- src/Components/Homepage/Home.jsx
+++ src/Components/Homepage/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../Header/MainHeader/Header'
 import CoinList from '../CoinList/CoinList'
+import Footer from '../Footer/Footer'
 
 const Home = () => {
     return (
         <div>
             <Header />
             <CoinList />
+            <Footer />
         </div>
     )
 }
