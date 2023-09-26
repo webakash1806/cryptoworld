f92e0e9d2e68ce060d3a3715343952746777ae8c
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-09-26 20:57:28 +0530

    Single Exchange container created

--- src/Components/SingleExchange/ExchangeData.jsx
+++ src/Components/SingleExchange/ExchangeData.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../Header/MainHeader/Header'
 import ExchangeChart from './ExchangeChart/ExchangeChart'
 import useSingleExchange from '../../Hooks/useSingleExchange'
+import LeftSectionContainer from './SingleExchangeContainer/LeftSectionContainer'
 import { useParams } from 'react-router-dom'
 
 const ExchangeData = () => {
@@ -15,6 +16,7 @@ const ExchangeData = () => {
         <div>
             <Header />
             <div className='pt-12'>{id} data coming soon...</div>
+            <LeftSectionContainer />
             <ExchangeChart id={id} />
         </div>
     )
---
+++ src/Components/SingleExchange/SingleExchangeContainer/LeftSectionContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LeftSectionContainer = () => {
+    return (
+        <div>
+            left
+        </div>
+    )
+}
+
+export default LeftSectionContainer
