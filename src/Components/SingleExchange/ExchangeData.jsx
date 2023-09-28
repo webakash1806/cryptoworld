2e544b5fc22ea4f4887be59dbcda1319092b8d2a
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-09-28 13:14:10 +0530

    Tickers List created in single exchange

--- src/Components/SingleExchange/ExchangeData.jsx
+++ src/Components/SingleExchange/ExchangeData.jsx
@@ -41,7 +41,19 @@ const ExchangeData = () => {
                         <ExchangeChart id={id} name={name} />
                     </div>
                 </div>
-                {exchangeData.tickers.map((val, ind) => <TickersList key={val.volume} rank={ind + 1} />)}
+                <div className="flex flex-col min-w-[19rem] max-w-[9.5rem] sm:max-w-[22rem] md:max-w-[26rem] lg:max-w-full lg:overflow-visible overflow-x-scroll">
+                    <div className="flex lg:sticky top-0 bg-white dark:bg-darkBg lg:gap-3 font-bold text-[14px] sm:text-[17x] capitalize border-t-[1px] border-b-[1px] border-[#b5b5b580] dark:border-[#6f6f6fab] pt-4 pb-4 w-fit">
+                        <p className="min-w-[2rem]  md:min-w-[5rem] text-center ">#</p>
+                        <p className='min-w-[10rem]  md:min-w-[11rem] text-left pl-1'>Pair</p>
+                        <p className='min-w-[7.5rem]  md:min-w-[9rem] text-right '>Price</p>
+                        <p className='min-w-[6rem]  md:min-w-[7rem] text-center '>Spread</p>
+                        <p className='min-w-[12rem]  md:min-w-[14rem] text-right pr-2 '>Volume</p>
+                        <p className='min-w-[5rem]  md:min-w-[5.5rem] text-center '>Score</p>
+                    </div>
+                    <div>
+                        {exchangeData.tickers.map((val, ind) => <TickersList key={val.volume} rank={ind + 1} score={val.trust_score} volume={val.converted_volume.btc * btcPrice} price={val.converted_last.btc * btcPrice} spread={val.bid_ask_spread_percentage} base={val.base} target={val.target} />)}
+                    </div>
+                </div>
             </>}
         </div>
     )
