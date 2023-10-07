cabab938922efaaf5e4e38f9d744803dd47cd626
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-10-08 01:17:37 +0530

    api endpoint added

--- src/Hooks/useApiEndpoints.js
+++ src/Hooks/useApiEndpoints.js
@@ -14,18 +14,27 @@ export const TrendingDataAPI = () => {
     return `https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`
 }
 
+// Single Coin data Endpoint
 export const singleCoinData = (id) => {
     return `https://api.coingecko.com/api/v3/coins/${id}?sparkline=false`
 }
 
+// Coin sparkline API Endpoint
 export const coinSparkLine = (id, days = 1) => {
     return `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=INR&days=${days}`
 }
 
+// Single Exchange data Endpoint
 export const singleExchange = (id) => {
     return `https://api.coingecko.com/api/v3/exchanges/${id}?sparkline=false`
 }
 
+// Single Exchange Sparkline
 export const exchangeSparkline = (id, day = 1) => {
     return `https://api.coingecko.com/api/v3/exchanges/${id}/volume_chart?vs_currency=INR&days=${day}`
+}
+
+// Search Endpoint
+export const searchEndpoint = (query) => {
+    return `https://api.coingecko.com/api/v3/search?query=${query}`
 }
