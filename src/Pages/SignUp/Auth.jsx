d1f2cc9fdd284cceb6aeaecca5b5afc95d6d9f7b
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-10-23 09:35:52 +0530

    signup page setup done

--- src/Pages/SignUp/Auth.jsx
+++ src/Pages/SignUp/Auth.jsx
@@ -1,11 +1,15 @@
 import Login from "./Login"
 import SignUp from "./SignUp"
+import Header from "../../Components/Header/MainHeader/Header"
+import Footer from "../../Components/Footer/Footer"
 
 const Auth = () => {
     return (
-        <div>
+        <div className="flex w-[100vw] flex-col justify-between h-[100vh]">
+            <Header />
             <Login />
             <SignUp />
+            <Footer />
         </div>
     )
 }
